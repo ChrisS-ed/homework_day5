732573d83ce8900c67f29a93643573a58103a462
ChrisS-ed <c.sloane@hotmail.co.uk>
2015-11-20 19:40:12 +0000

    first two functions working

--- warehouse_picker_functions.rb
+++ warehouse_picker_functions.rb
@@ -1,6 +1,12 @@
 
-WAREHOUSE = [{bay: "a10", item: nil}, {bay: "a9", item: nil}, {bay: "a8", item: nil}, {bay: "a7", item: nil}, {bay: "a6", item: nil}, {bay: "a5", item: nil}, {bay: "a4", item: nil}, {bay: "a3", item: nil}, {bay: "a2", item: nil}, {bay: "a1", item: nil}, {bay: "c1", item: nil}, {bay: "c2", item: nil}, {bay: "c3", item: nil}, {bay: "c4", item: nil}, {bay: "c5", item: nil}, {bay: "c6", item: nil}, {bay: "c7", item: nil}, {bay: "c8", item: nil}, {bay: "c9", item: nil}, {bay: "c10", item: nil}, {bay: "b1", item: nil}, {bay: "b2", item: nil}, {bay: "b3", item: nil}, {bay: "b4", item: nil}, {bay: "b5", item: nil}, {bay: "b6", item: nil}, {bay: "b7", item: "bath fizzers"}, {bay: "b8", item: nil}, {bay: "b9", item: nil}, {bay: "b10", item: nil}]
+WAREHOUSE = [{bay: "a10", item: "rubber band"}, {bay: "a9", item: "glow stick"}, {bay: "a8", item: "model car"}, {bay: "a7", item: "bookmark"}, {bay: "a6", item: "shovel"}, {bay: "a5", item: "rubber duck"}, {bay: "a4", item: "hanger"}, {bay: "a3", item: "blouse"}, {bay: "a2", item: "stop sign"}, {bay: "a1", item: "needle"}, 
+            {bay: "c1", item: "rusty nail"}, {bay: "c2", item: "drill press"}, {bay: "c3", item: "chalk"}, {bay: "c4", item: "word search"}, {bay: "c5", item: "thermometer"}, {bay: "c6", item: "face wash"}, {bay: "c7", item: "paint brush"}, {bay: "c8", item: "candy wrapper"}, {bay: "c9", item: "shoe lace"}, {bay: "c10", item: "leg warmers"}, 
+            {bay: "b1", item: "tyre swing"}, {bay: "b2", item: "sharpie"}, {bay: "b3", item: "picture frame"}, {bay: "b4", item: "photo album"}, {bay: "b5", item: "nail filer"}, {bay: "b6", item: "tooth paste"}, {bay: "b7", item: "bath fizzers"}, {bay: "b8", item: "tissue box"}, {bay: "b9", item: "deodorant"}, {bay: "b10", item: "cookie jar"}]
 
 def return_item_at_bay(chosen_bay)
   WAREHOUSE.select { |location| location[:bay] == chosen_bay }.first[:item]
+end
+
+def return_bay_with_item(chosen_item)
+  WAREHOUSE.select { |wanted| wanted[:item] == chosen_item }.first[:bay]
 end
--- warehouse_picker_runner.rb
+++ warehouse_picker_runner.rb
@@ -1,15 +1,14 @@
 require_relative './warehouse_picker_functions'
 
-# warehouse picker
-# warehouse is array of three racks: [row_a, row_b, row_c]
-# each rack has 10 bays which can each contain items: row_a = {a1: item, a2: item .. a10: item} 
-
-
 # Given a bay, returns the item in that bay
-bay = "b7"
-found_item = return_item_at_bay(bay)
-puts "The item at bay #{bay} is #{found_item}"
+# print "Please enter the bay you would like an item from: "
+# bay = gets.chomp
+# found_item = return_item_at_bay(bay)
+# puts "The item at bay #{bay} is #{found_item}"
 
 # Given an item return the bay that it is in.
-# found_bay = return_bay_with_item(item)
-# puts "The bay containing item #{item} is bay #{bay}"
+print "Please enter the name of an item to find out what bay it is in: "
+item = gets.chomp
+found_bay = return_bay_with_item(item)
+puts "Bay #{found_bay} contains #{item}"
+
