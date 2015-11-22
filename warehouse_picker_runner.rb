732c36337af826d18b4c157ef424e5e4449a4ed5
ChrisS-ed <c.sloane@hotmail.co.uk>
2015-11-22 16:27:18 +0000

    tidied up output text

--- warehouse_picker_runner.rb
+++ warehouse_picker_runner.rb
@@ -4,15 +4,17 @@ require_relative './warehouse_picker_functions'
 
 # print "Please enter the bay you would like an item from: "
 # bay = gets.chomp
-# found_item = return_item_at_bay(bay)
-# puts "The item at bay #{bay} is #{found_item}"
+bay = "a5"
+found_item = return_item_at_bay(bay)
+puts "The item at bay #{bay} is #{found_item}"
 
 # Given an item return the bay that it is in.
 
 # print "Please enter the name of an item to find out what bay it is in: "
 # item = gets.chomp
-# found_bay = return_bay_with_item(item)
-# puts "Bay #{found_bay} contains #{item}"
+item = "paint brush"
+found_bay = return_bay_with_item(item)
+puts "Bay #{found_bay} contains #{item}"
 
 #Given a list of bays, list the items in those bays
 
@@ -30,18 +32,23 @@ list_of_bays = ["a10", "b5", "c10"]
 #   end
 # end
 found_items = return_list_of_items(list_of_bays)
-puts found_items
+puts "Found items: #{found_items} for bays: #{list_of_bays}"
 
 # Given a list of items find the bays.
 list_of_items = ["rubber band", "nail filer", "leg warmers"]
 found_bays = return_list_of_bays(list_of_items)
-puts found_bays
+puts "Found bays: #{found_bays} for items: #{list_of_items}"
 
 # Given a list of bays, list the items in those bays, and calculate the distance from the two furthest apart bays. 
-list_of_bays = ["b3", "c7", "c9", "a3"]
+list_of_bays = ["b5", "b10", "b6"]
 found_items = return_list_of_items(list_of_bays)
 furthest_bays_distance = find_furthest_distance(list_of_bays)
 puts "The biggest distance between two bays is #{furthest_bays_distance}"
+
+# Given a list of products, find the bays that need to be visited, and order them in the order they need to be visited from entrance to exit
+list_of_items = ["shoe lace", "rusty nail", "leg warmers"]
+found_bays = return_list_of_bays(list_of_items)
+puts "Found bays: #{found_bays} for items: #{list_of_items}"
 
 
 
